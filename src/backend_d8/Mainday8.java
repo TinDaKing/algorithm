ce3eee40a67ead49ff0a37502bd7bff12b862840
TinDaKing <tranhoangtin3702@gmail.com>
2022-10-25 21:34:33 +0700

    update main

--- src/backend_d8/Mainday8.java
+++ src/backend_d8/Mainday8.java
@@ -18,7 +18,7 @@ public class Mainday8 {
 		// task 4
 		int[] arr = { 1, 2, 2, 0, 0, 1, 2, 2, 1 };
 		int[] sortedArr = solve.sortAlgorithm(arr);
-		System.out.println(solve.isSortedArray(sortedArr));
+		System.out.println(solve.isSortedArray(sortedArr)); // TDD
 		solve.printArray(sortedArr);
 	}
 
