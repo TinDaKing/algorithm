ef55c6313ef5e0858a062d5356dba7dd7c6bc812
TinDaKing <tranhoangtin3702@gmail.com>
2022-10-25 21:32:13 +0700

    all implements commit

---
+++ src/backend_d8/Mainday8.java
@@ -0,0 +1,25 @@
+package backend_d8;
+
+import solutions.*;
+
+public class Mainday8 {
+
+	public static void main(String[] args) {
+
+		Solutions solve = new Solutions();
+
+		// task 1
+		System.out.println(solve.sqrtFunc(2077));
+		// task 2
+		System.out.println(solve.romanNumber("CMLXXIX"));
+		// task 3
+		String[] strs = { "flower", "flow", "florida", "flight" };
+		System.out.println(solve.LongestSameString(strs));
+		// task 4
+		int[] arr = { 1, 2, 2, 0, 0, 1, 2, 2, 1 };
+		int[] sortedArr = solve.sortAlgorithm(arr);
+		System.out.println(solve.isSortedArray(sortedArr));
+		solve.printArray(sortedArr);
+	}
+
+}
